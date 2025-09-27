6afc44871a8b20f74f6501d411f8f6c5ef2fb9c7
Celso Bonutti <i.am@cel.so>
2025-09-27 13:53:51 -0300

    cleanup

--- Funny/Basic.lean
+++
@@ -1,9 +0,0 @@
-def Filter (list : List α) : Type :=
-  if list.length >= 1
-  then Unit
-  else Empty
-
-def withNotEmpty (list : List α) (_ : Filter list) (f : List α → β) : β := f list
-
-#eval withNotEmpty [1, 2, 3, 4] () List.sum
-#eval withNotEmpty [] _ <| List.map (Nat.add 1)
--- Main.lean
+++ Main.lean
@@ -1,4 +1,5 @@
-import Funny
+import Funny.Chap3.Terms
+import Funny.Chap4.Terms
 
 def main : IO Unit :=
   IO.println s!"Hello, I ran!"
